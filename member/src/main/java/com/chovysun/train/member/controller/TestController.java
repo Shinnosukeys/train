7833b276e1543fa26e156142be5385231424bdb7
Shinnosukeys <sunchy8@mail.ustc.edu.cn>
2025-05-20 01:45:12 +0800

    feat: nacos配置

--- member/src/main/java/com/chovysun/train/member/controller/TestController.java
+++ member/src/main/java/com/chovysun/train/member/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.chovysun.train.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class TestController {
 
+    @Value("${test.nacos}")
+    private String testNacos;
+
     @Autowired
     Environment environment;
 
     @GetMapping("/hello")
     public String hello() {
         String port = environment.getProperty("local.server.port");
-        return String.format("Hello! 端口：%s", port);
+        return String.format("Hello %s! 端口：%s", testNacos, port);
     }
 }
